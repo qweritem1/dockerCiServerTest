83a3e9d640caafdf071b324aaef4e96d3565637f
1003642 <a1003642@SKP1003642MN002.local>
2018-07-15 23:38:08 +0900

    First commit

---
+++ src/main/java/com/dosjung/dockerCiServerTest/DockerCiServerTestApplication.java
@@ -0,0 +1,12 @@
+package com.dosjung.dockerCiServerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerCiServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerCiServerTestApplication.class, args);
+	}
+}
